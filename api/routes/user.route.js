6e197bbde4f98162719b3f1e352b2161bc9c83be
MOHAMMAD ANAS <anasmohd2507@gmail.com>
2024-03-19 10:33:58 +0530

    create sign in api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import test from "../controllers/user.controller.js";
-import signup from "../controllers/auth.controller.js";
+import { signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
