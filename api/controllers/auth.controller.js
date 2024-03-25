468d344fa17dbe82576e8c6fc7d0801342d11119
MOHAMMAD ANAS <anasmohd2507@gmail.com>
2024-03-25 13:05:18 +0530

    add sign out functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -82,4 +82,13 @@ const google = async (req, res, next) => {
   }
 };
 
-export { signup, signin, google };
+const signOut = (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json("User has been logged out!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { signup, signin, google, signOut };
